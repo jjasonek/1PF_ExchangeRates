cdb9fae65398c11ec4424adf36eba16aca1c2c45
jjasonek <ji.jas@seznam.cz>
2024-02-16 14:35:34 +0100

    Add negative test scenarios for the ExchangeRatesControllerTest

--- ExchangeRatesBE/src/main/java/cz/firstpf/exchangerates/controller/ExchangeRatesExceptionHandler.java
+++ ExchangeRatesBE/src/main/java/cz/firstpf/exchangerates/controller/ExchangeRatesExceptionHandler.java
@@ -1,5 +1,6 @@
 package cz.firstpf.exchangerates.controller;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,8 @@ public final class ExchangeRatesExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<String> handleException(Exception exception) {
+        String stacktrace = ExceptionUtils.getStackTrace(exception);
+        log.error("unexpected error caused by " + stacktrace);
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- ExchangeRatesBE/src/test/java/cz/firstpf/exchangerates/controller/ExchangeRatesControllerTest.java
+++ ExchangeRatesBE/src/test/java/cz/firstpf/exchangerates/controller/ExchangeRatesControllerTest.java
@@ -12,14 +12,21 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * Web layer tests for {@link ExchangeRatesController}
+ *
+ * @author Jiri Jasonek
+ */
+
 @WebMvcTest(ExchangeRatesController.class)
 @ContextConfiguration(classes = ExchangeRatesApplication.class)
-class ExchangeRatesControllerTest {
+final class ExchangeRatesControllerTest {
 
     private static final String EXCHANGE_RATES = "/exchangerates/api/v1";
 
@@ -30,13 +37,37 @@ class ExchangeRatesControllerTest {
 
     @Test
     void getExchangeRates_success() throws Exception {
-        when(exchangeRatesService.getExchangeRates(true)).thenReturn(new ArrayList<>());
+        when(exchangeRatesService.getExchangeRates(anyBoolean())).thenReturn(new ArrayList<>());
+
         this.mockMvc.perform(get(EXCHANGE_RATES)
                         .param("useDb", String.valueOf(true))
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().string("[]"));
-//                .andExpect(jsonPath("$").value(Collections.EMPTY_LIST));
+    }
+
+    @Test
+    void getExchangeRates_failsOnException() throws Exception {
+        when(exchangeRatesService.getExchangeRates(anyBoolean())).thenThrow(new RuntimeException(""));
+
+        this.mockMvc.perform(get(EXCHANGE_RATES)
+                        .param("useDb", String.valueOf(true))
+                )
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    void getExchangeRates_failsOnMissingRequestParameter() throws Exception {
+
+        this.mockMvc.perform(get(EXCHANGE_RATES))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    void getExchangeRates_failsOnBadUrl() throws Exception {
+
+        this.mockMvc.perform(get(""))
+                .andExpect(status().isInternalServerError());
     }
 }
