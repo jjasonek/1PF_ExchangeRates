eeb05560dbc52e67d38676ef5b7224666b27fbf2
jjasonek <ji.jas@seznam.cz>
2024-02-19 02:58:08 +0100

    Add component exchange-rates

---
+++ ExchangeRatesFE/src/app/exchange-rates/exchange-rates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exchange-rates',
+  standalone: true,
+  imports: [],
+  templateUrl: './exchange-rates.component.html',
+  styleUrl: './exchange-rates.component.css'
+})
+export class ExchangeRatesComponent {
+
+}
