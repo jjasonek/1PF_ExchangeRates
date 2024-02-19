42caca6db206002819168ccb4879ea75c75d9493
jjasonek <ji.jas@seznam.cz>
2024-02-19 05:11:26 +0100

    Add ExchangeRate interface

---
+++ ExchangeRatesFE/src/app/models/exchange-rate.ts
@@ -0,0 +1,17 @@
+export interface ExchangeRate {
+  shortName: string,
+  validFrom: Date,
+  name: string,
+  country: string
+  move: number,
+  amount: number,
+  valBuy: number,
+  valSell: number,
+  valMid: number,
+  currBuy: number,
+  currSell: number,
+  currMid: number,
+  version: number,
+  cnbMid: number,
+  ecbMid: number
+}
