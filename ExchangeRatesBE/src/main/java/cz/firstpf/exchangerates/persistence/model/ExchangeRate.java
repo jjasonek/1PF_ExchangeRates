e62bf2db80042350d1247ca7a05feed69dd4e070
jjasonek <ji.jas@seznam.cz>
2024-02-29 22:57:22 +0100

    Remove unnecessary Serializable

--- ExchangeRatesBE/src/main/java/cz/firstpf/exchangerates/persistence/model/ExchangeRate.java
+++ ExchangeRatesBE/src/main/java/cz/firstpf/exchangerates/persistence/model/ExchangeRate.java
@@ -5,12 +5,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -31,11 +29,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Table(name = "exchange_rates")
 @Entity
-public final class ExchangeRate implements Serializable {
+public final class ExchangeRate {
 
     @Id
     private long hash;
-
     private String shortName;
     private LocalDateTime validFrom;
     private String name;
